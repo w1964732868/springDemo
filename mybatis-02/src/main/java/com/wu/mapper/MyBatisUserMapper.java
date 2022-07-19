6ca384887e9277af9b2d116fd2d0fa8b3c6734c5
huijie.wu <1964732868@qq.com>
2022-07-19 13:24:20 +0800

    java select注解  RowBounds

--- mybatis-02/src/main/java/com/wu/mapper/MyBatisUserMapper.java
+++ mybatis-02/src/main/java/com/wu/mapper/MyBatisUserMapper.java
@@ -2,6 +2,7 @@ package com.wu.mapper;
 
 import com.wu.pojo.User;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Map;
@@ -12,5 +13,10 @@ public interface MyBatisUserMapper {
     User getUserById(@Param("map") Map<String, Object> map);
 
     User getUserById1(@Param("id") int id);
+
+    List<User> getUserListLimit(@Param("map") Map<String, Integer> map);
+
+    @Select("select id,name,pwd as password from mysql.user")
+    User getUserLimit();
 
 }
--- mybatis-02/src/test/java/MyTest.java
+++ mybatis-02/src/test/java/MyTest.java
@@ -1,11 +1,13 @@
 import com.wu.mapper.MyBatisUserMapper;
 import com.wu.pojo.User;
 import com.wu.utils.MybatisUtils;
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -47,6 +49,47 @@ public class MyTest {
         //User(id=1, name=哈哈, password=111)
         //User(id=1, name=哈哈, password=111)
         //关闭
+        sqlSession.close();
+    }
+
+    //总共4条
+    //select * from mysql.user limit 0,2; -- 1,2
+    //select * from mysql.user limit 1,2; -- 2,3
+    //select * from mysql.user limit 2,2; -- 3,4
+
+    //select * from mysql.user limit 3; -- 1,2,3
+
+    //sql limit
+    @Test
+    public void getUserListLimit() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        MyBatisUserMapper mapper = sqlSession.getMapper(MyBatisUserMapper.class);
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("startIndex", 1);
+        map.put("pageSize", 2);
+        List<User> userList = mapper.getUserListLimit(map);
+        for (User user : userList) {
+            System.out.println(user);
+            //User(id=2, name=hehe, password=1)
+            //User(id=3, name=keke, password=2)
+        }
+
+        sqlSession.close();
+    }
+
+
+    //注解
+    @Test
+    public void getUserLimit() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        RowBounds rowBounds = new RowBounds(0, 2);//1 2
+        List<User> selectList = sqlSession.selectList("com.wu.mapper.MyBatisUserMapper.getUserLimit", "", rowBounds);
+        for (User user : selectList) {
+            System.out.println(user);
+            //User(id=1, name=哈哈, password=111)
+            //User(id=2, name=hehe, password=1)
+        }
+
         sqlSession.close();
     }
 
